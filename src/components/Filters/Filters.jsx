fd5e4c343604ec05a48bedbd6cbdfac8524b81a8
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-12-11 13:18:02 +0530

    Filters comp Done

---
+++ src/components/Filters/Filters.jsx
@@ -0,0 +1,23 @@
+const Filters = () => {
+  return (
+    <div
+      className="w-full bg-white rounded-lg p-5"
+      style={{ height: "max-content" }}
+    >
+      <li className="font-poppins font-semibold text-base  py-4 list-none text-primary">
+        Tweets
+      </li>
+      <li className="font-poppins font-semibold text-base text-gray-600 py-4 list-none ">
+        Tweets & Replies
+      </li>
+      <li className="font-poppins font-semibold text-base text-gray-600 py-4 list-none ">
+        Media
+      </li>
+      <li className="font-poppins font-semibold text-base text-gray-600 py-4 list-none ">
+        Likes
+      </li>
+    </div>
+  );
+};
+
+export default Filters;
