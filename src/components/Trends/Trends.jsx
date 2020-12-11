61ef817a6e922990dc30593d449c569df6ec9b45
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-12-11 13:18:29 +0530

    Trends comp Done

---
+++ src/components/Trends/Trends.jsx
@@ -0,0 +1,41 @@
+const Trends = () => {
+  return (
+    <div
+      className="bg-white w-full p-5 rounded-lg"
+      style={{ height: "max-content" }}
+    >
+      <p className="font-poppins font-semibold text-base mb-3">Trends</p>
+      <hr />
+      <div className="tags">
+        <p className="py-2 font-noto font-semibold">#programming</p>
+        <p className="font-noto font-medium text-gray-600">213k Tweets</p>
+      </div>
+      <div className="tags">
+        <p className="py-2 font-noto font-semibold">#dev</p>
+        <p className="font-noto font-medium text-gray-600">3k Tweets</p>
+      </div>
+      <div className="tags">
+        <p className="py-2 font-noto font-semibold">#monday</p>
+        <p className="font-noto font-medium text-gray-600">21.3k Tweets</p>
+      </div>
+      <div className="tags">
+        <p className="py-2 font-noto font-semibold">#frontend</p>
+        <p className="font-noto font-medium text-gray-600">33k Tweets</p>
+      </div>
+      <div className="tags">
+        <p className="py-2 font-noto font-semibold">#oneplus</p>
+        <p className="font-noto font-medium text-gray-600">213k Tweets</p>
+      </div>
+      <div className="tags">
+        <p className="py-2 font-noto font-semibold">#programming</p>
+        <p className="font-noto font-medium text-gray-600">25k Tweets</p>
+      </div>
+      <div className="tags">
+        <p className="py-2 font-noto font-semibold">#memes</p>
+        <p className="font-noto font-medium text-gray-600">213k Tweets</p>
+      </div>
+    </div>
+  );
+};
+
+export default Trends;
