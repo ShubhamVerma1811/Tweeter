d8cc50ab15d6cfb0c01cae4a768ef9a816ae7c67
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-12-11 13:26:56 +0530

    Explore Filters Comp done

---
+++ src/components/ExploreFIlters/ExploreFilters.jsx
@@ -0,0 +1,23 @@
+const ExploreFilters = () => {
+  return (
+    <div
+      className="w-full bg-white rounded-lg p-5"
+      style={{ height: "max-content" }}
+    >
+      <li className="font-poppins font-semibold text-base  py-4 list-none text-primary">
+        Top
+      </li>
+      <li className="font-poppins font-semibold text-base text-gray-600 py-4 list-none ">
+        Latest
+      </li>
+      <li className="font-poppins font-semibold text-base text-gray-600 py-4 list-none ">
+        People
+      </li>
+      <li className="font-poppins font-semibold text-base text-gray-600 py-4 list-none ">
+        Media
+      </li>
+    </div>
+  );
+};
+
+export default ExploreFilters;
