103322dfea2037fd3635e73c49c93d9ea460bfb2
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-12-12 14:26:52 +0530

    clickable posts,

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -5,7 +5,8 @@ import React, { useEffect, useState } from "react";
 import ExploreFilters from "../components/ExploreFIlters/ExploreFilters";
 import Post from "../components/Post/Post";
 import Layout from "../layouts";
-import { fetchUser } from "../services/FetchUser";
+import { fetchUser } from "../services/FetchData";
+import Link from "next/link";
 
 const Explore = () => {
   const [exploreTweets, setExploreTweets] = useState([]);
@@ -40,29 +41,32 @@ const Explore = () => {
       <Head>
         <title>Explore | Tweeter</title>
       </Head>
-      <Layout />
-      <div className="mx-4 sm:mx-12 md:mx-24 lg:mx-48 mt-5">
-        <div className="flex flex-col lg:grid lg:grid-cols-3 lg:col-gap-5">
-          <div className="hidden lg:block lg:col-span-1">
-            <div className="mb-5">
-              <ExploreFilters />
+      <Layout>
+        <div className="mx-4 sm:mx-12 md:mx-24 lg:mx-48 mt-5">
+          <div className="flex flex-col lg:grid lg:grid-cols-3 lg:col-gap-5">
+            <div className="hidden lg:block lg:col-span-1">
+              <div className="mb-5">
+                <ExploreFilters />
+              </div>
+            </div>
+            <div className="lg:col-span-2">
+              {exploreTweets && exploreTweets.length > 0 ? (
+                exploreTweets.map((tweet) => (
+                  <Link href={`${tweet.author.username}/status/${tweet.id}`}>
+                    <div className="mb-5">
+                      <Post tweet={tweet} />
+                    </div>
+                  </Link>
+                ))
+              ) : (
+                <div className="flex justify-center">
+                  <CircularProgress />
+                </div>
+              )}
             </div>
           </div>
-          <div className="lg:col-span-2">
-            {exploreTweets && exploreTweets.length > 0 ? (
-              exploreTweets.map((tweet) => (
-                <div className="mb-5">
-                  <Post tweet={tweet} />
-                </div>
-              ))
-            ) : (
-              <div className="flex justify-center">
-                <CircularProgress />
-              </div>
-            )}
-          </div>
         </div>
-      </div>
+      </Layout>
     </div>
   );
 };
