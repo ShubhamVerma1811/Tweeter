06a02bae5191edf75183d1e37d95e985a2876d16
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-12-12 14:30:54 +0530

    prettier fixes

--- src/components/CommentInput/CommentInput.jsx
+++ src/components/CommentInput/CommentInput.jsx
@@ -8,20 +8,20 @@ const CommentInput = ({ tweetID }) => {
   const [comment, setComment] = useState(null);
 
   return (
-    <div>
+    <div className="bg-white">
       <div className="flex flex-row items-center">
-        <div className="w-16 overflow-hidden rounded-lg m-4">
+        <div className="w-12 h-12 overflow-hidden rounded-lg m-4">
           <Avatar src={user && user.profilePicture} />
         </div>
         <div className="w-full">
           <form
+            className="px-4"
             onSubmit={(e) => {
               e.preventDefault();
-              postTweet(user.uid, comment, tweetID);
-            }}
-          >
+              postTweet(user.uid, comment, null, tweetID);
+            }}>
             <input
-              className="bg-gray-200 placeholder-gray-600 px-4 rounded-lg h-12 w-full font-noto text-sm font-medium"
+              className="bg-gray-200 placeholder-gray-600  rounded-lg h-12 w-full font-noto text-sm font-medium"
               type="text"
               placeholder="Tweet your Reply"
               onChange={(e) => setComment(e.target.value)}
