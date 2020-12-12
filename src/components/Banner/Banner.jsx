4a6c4420969e815074b92ff0e20fd7e0ec82bda6
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-12-12 14:21:01 +0530

    prettier fix

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,8 +4,7 @@ const Banner = () => {
       className="overflow-hidden"
       style={{
         height: "300px",
-      }}
-    >
+      }}>
       <img
         className="w-full"
         src="https://images.unsplash.com/photo-1522439748419-3cd697a86028?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
