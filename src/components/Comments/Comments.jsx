853586cc9b43afec2cbb2fce7c860381697fa7c8
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-12-12 14:30:38 +0530

    Added:login to load comments of tweet

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,48 +1,80 @@
-import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
-import { useEffect } from "react";
-import Avatar from "../Avatar/Avatar";
 import firebase from "firebase";
+import { useEffect, useState } from "react";
+import { fetchUser } from "../../services/FetchData";
+import Avatar from "../Avatar/Avatar";
 
 const Comments = ({ tweetID }) => {
+  const [comments, setComments] = useState([]);
+
   useEffect(() => {
-    firebase.firestore();
+    const getComments = async () => {
+      const tweetsRef = await firebase
+        .firestore()
+        .collection("tweets")
+        .where("parentTweet", "==", tweetID)
+        .get();
+      const localComments = [];
+
+      for (let i = 0; i < tweetsRef.size; i++) {
+        const tweet = tweetsRef.docs[i].data();
+        const id = tweetsRef.docs[i].id;
+        const userInfo = await fetchUser({
+          userID: tweet.authorId,
+        });
+        localComments.push({
+          ...tweet,
+          id,
+          createdAt: tweet.createdAt.toDate().toString(),
+          author: userInfo,
+        });
+      }
+
+      setComments(localComments);
+    };
+
+    getComments();
   }, []);
 
   return (
-    <div className="rounded-lg">
-      <div className="flex flex-row">
-        <div className="m-2">
-          <Avatar src="" />
-        </div>
-        <div className="flex flex-col  w-full">
-          <div className="bg-gray-200 p-4">
-            <div className="p-2">
-              <p className="inline-block font-poppins font-medium">
-                Waqar Bloom
-              </p>
-              <p className="inline-block font-noto font-medium text-xs text-gray-600">
-                24 August at 20:43{" "}
-              </p>
-            </div>
-            <div className="p-2">
-              <p className="font-noto text-gray-700">
-                I’ve seen awe-inspiring things that I thought I’d never be able
-                to explain to another person.
-              </p>
+    <div className="bg-white rounded-b-lg">
+      {comments &&
+        comments.length > 0 &&
+        comments.map((comment) => (
+          <div className="rounded-lg" key={comment.id}>
+            <div className="flex flex-row">
+              <div className="m-2 w-12 h-12">
+                <Avatar src={comment.author.profilePicture} />
+              </div>
+              <div className="flex flex-col  w-full">
+                <div className="bg-gray-200 p-4">
+                  <div className="p-2">
+                    <p className="inline-block font-poppins font-medium">
+                      {comment.author.name}
+                    </p>
+                    <p className="inline-block font-noto font-medium text-xs text-gray-600">
+                      {comment.createdAt}{" "}
+                    </p>
+                  </div>
+                  <div className="p-2">
+                    <p className="font-noto text-gray-700">{comment.text}</p>
+                  </div>
+                </div>
+                {/* <div className="flex flex-row py-4">
+                  <button type="submit">
+                    <span>
+                      <FavoriteBorderIcon />
+                    </span>
+                    Like
+                  </button>
+                  <p className="px-2">.</p>
+                  <p className="font-noto font-semibold text-gray-400">
+                    12k Likes
+                  </p>
+                </div> */}
+              </div>
             </div>
           </div>
-          <div className="flex flex-row py-4">
-            <button type="submit">
-              <span>
-                <FavoriteBorderIcon />
-              </span>
-              Like
-            </button>
-            <p className="px-2">.</p>
-            <p className="font-noto font-semibold text-gray-400">12k Likes</p>
-          </div>
-        </div>
-      </div>
+        ))}
     </div>
   );
 };
