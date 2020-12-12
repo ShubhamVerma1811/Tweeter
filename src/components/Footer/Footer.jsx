50320f984d0863d7035fab93c6cb46b029e82298
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-12-12 14:17:46 +0530

    Added Footer Comp

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="text-sm lg:text-base font-poppins font-semibold text-secondary flex flex-col justify-center items-center p-4">
+      <p className="lg:mr-2">
+        Made by{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline cursor-pointer text-gray-900"
+          href="https://shubhamverma.me">
+          Shubham Verma
+        </a>
+      </p>
+      <p className="block">Design was inspired by DevChallnges.io</p>
+    </div>
+  );
+};
+
+export default Footer;
