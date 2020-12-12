950308b21ade57284a44f09a1b221fef1686cac7
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-12-12 14:20:38 +0530

    rem: static code add:loader, fetch user

--- src/components/Suggestions/Suggestions.jsx
+++ src/components/Suggestions/Suggestions.jsx
@@ -1,89 +1,71 @@
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+import { useEffect, useState } from "react";
+import { fetchUser, fetchUserFollowers } from "../../services/FetchData";
 import Avatar from "../Avatar/Avatar";
 
 const Suggestions = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(async () => {
+    const localUser = await fetchUser({
+      userID: "RLxWOdfowdVStsGBI6rDWDoaXCZ2",
+    });
+    const followersCount = await fetchUserFollowers(localUser.uid);
+    setUser({ ...localUser, followersCount: followersCount.size });
+    setLoading(false);
+  }, []);
+
   return (
     <div
       className="bg-white w-full p-5 rounded-lg"
-      style={{ height: "max-content" }}
+      // style={{ height: "max-content" }}
     >
       <p className="font-poppins font-semibold text-base mb-3">Who to Follow</p>
       <hr />
-      <div>
-        <div className="flex flex-row my-4">
-          <div className="w-10 mr-4">
-            <Avatar src="https://uifaces.co/our-content/donated/gPZwCbdS.jpg" />
+      {loading && (
+        <div className="flex justify-center">
+          <CircularProgress />
+        </div>
+      )}
+      {user && (
+        <div>
+          <div className="flex flex-row my-4">
+            <div className="w-12 h-12 mr-4">
+              <Avatar src={user.profilePicture} />
+            </div>
+            <div className="flex flex-col">
+              <p className="font-poppins font-medium">{user.name}</p>
+              <p className="font-noto font-medium text-sm text-gray-600">
+                {user.followersCount} Followers
+              </p>
+            </div>
+            <button
+              className="bg-primary mr-0 ml-auto text-white px-8 py-4 rounded-md lg:px-4 lg:py-2"
+              type="submit">
+              Follow
+            </button>
           </div>
-          <div className="flex flex-col">
-            <p className="font-poppins font-medium">Mikael Stanley</p>
-            <p className="font-noto font-medium text-sm text-gray-600">
-              230k followers
+          <div>
+            <p className="font-noto font-medium text-gray-600 my-4">
+              {user.bio}
             </p>
           </div>
-          <button
-            className="bg-primary mr-0 ml-auto text-white px-8 py-4 rounded-md lg:px-4 lg:py-2"
-            type="submit"
-          >
-            Follow
-          </button>
-        </div>
-        <div>
-          <p className="font-noto font-medium text-gray-600 my-4">
-            Photographer & Filmmaker based in Copenhagen, Denmark
-          </p>
-        </div>
-        <div
-          className="overflow-hidden rounded-lg"
-          style={{
-            height: "100px",
-            maxHeight: "300px",
-          }}
-        >
-          <img
-            className="w-full"
-            src="https://images.unsplash.com/photo-1522439748419-3cd697a86028?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-            alt="banner"
-          />
-        </div>
-      </div>
-      <hr className="my-4" />
-
-      <div>
-        <div className="flex flex-row my-4">
-          <div className="w-10 mr-4">
-            <Avatar src="https://uifaces.co/our-content/donated/gPZwCbdS.jpg" />
+          <div
+            className="overflow-hidden rounded-lg"
+            style={{
+              height: "100px",
+              maxHeight: "300px",
+            }}>
+            <img
+              className="w-full"
+              src="https://images.unsplash.com/photo-1522439748419-3cd697a86028?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+              alt="banner"
+            />
           </div>
-          <div className="flex flex-col">
-            <p className="font-poppins font-medium">Mikael Stanley</p>
-            <p className="font-noto font-medium text-sm text-gray-600">
-              230k followers
-            </p>
-          </div>
-          <button
-            className="bg-primary mr-0 ml-auto text-white px-8 py-4 rounded-md"
-            type="submit"
-          >
-            Follow
-          </button>
         </div>
-        <div>
-          <p className="font-noto font-medium text-gray-600 my-4">
-            Photographer & Filmmaker based in Copenhagen, Denmark
-          </p>
-        </div>
-        <div
-          className="overflow-hidden rounded-lg"
-          style={{
-            height: "100px",
-            maxHeight: "300px",
-          }}
-        >
-          <img
-            className="w-full"
-            src="https://images.unsplash.com/photo-1522439748419-3cd697a86028?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-            alt="banner"
-          />
-        </div>
-      </div>
+      )}
     </div>
   );
 };
