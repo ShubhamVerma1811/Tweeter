4cb6891cdca164186f8292d7c0d61e5e688a425b
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-12-12 14:28:36 +0530

    added spinner,logic for user's existance in DB

--- src/pages/[username]/index.jsx
+++ src/pages/[username]/index.jsx
@@ -1,46 +1,67 @@
 import Head from "next/head";
+import Link from "next/link";
+import { useState, useEffect } from "react";
 import Layout from "../..//layouts/index";
 import Banner from "../../components/Banner/Banner";
 import Filters from "../../components/Filters/Filters";
 import Post from "../../components/Post/Post";
 import UserInfo from "../../components/UserInfo/UserInfo";
-import fetchAllUserData from "../../services/FetchUser";
+import fetchAllUserData from "../../services/FetchData";
 
 const UserName = ({ fetchedUser, tweets }) => {
+  const [userExits, setUserExits] = useState(false);
+
+  useEffect(() => {
+    if (fetchedUser) {
+      setUserExits(true);
+    }
+  }, []);
+
   return (
     <div>
       <Head>
         <title>
-          {fetchedUser.name} (@{fetchedUser.username}) | Tweeter
+          {userExits
+            ? `${fetchedUser.name} (@${fetchedUser.username}) | Tweeter`
+            : `USER NOT FOUND`}
         </title>
       </Head>
 
-      <Layout />
-      <Banner />
-      <div
-        className="flex mx-6 sm:mx-12 md:mx-24 justify-center relative m-auto"
-        style={{
-          top: "-100px",
-        }}
-      >
-        <UserInfo fetchedUser={fetchedUser} />
-      </div>
-      <div className="mx-4 sm:mx-12 md:mx-24 m-auto">
-        <div className="flex flex-col lg:grid lg:grid-cols-3 lg:col-gap-5">
-          <div className="mb-5">
-            <Filters />
-          </div>
-          <div className="col-span-2">
-            {tweets.map((tweet) => {
-              return (
-                <div className="mb-5" key={tweet.id}>
-                  <Post tweet={tweet} />
+      <Layout>
+        {userExits ? (
+          <>
+            <Banner />
+            <div
+              className="flex mx-6 sm:mx-12 md:mx-24 justify-center relative m-auto"
+              style={{
+                top: "-100px",
+              }}>
+              <UserInfo fetchedUser={fetchedUser} />
+            </div>
+            <div className="mx-4 sm:mx-12 md:mx-24 m-auto">
+              <div className="flex flex-col lg:grid lg:grid-cols-3 lg:col-gap-5">
+                <div className="mb-5">
+                  <Filters />
                 </div>
-              );
-            })}
-          </div>
-        </div>
-      </div>
+                <div className="col-span-2">
+                  {tweets.map((tweet) => {
+                    return (
+                      <Link
+                        href={`${tweet.author.username}/status/${tweet.id}`}>
+                        <div className="mb-5" key={tweet.id}>
+                          <Post tweet={tweet} />
+                        </div>
+                      </Link>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <h1>User Not Found</h1>
+        )}
+      </Layout>
     </div>
   );
 };
