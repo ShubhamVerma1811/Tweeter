fe1e4887320f76a61e13a20f61d5768197c9db2a
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-12-13 06:43:30 +0530

    Added bottom margin for small screens

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="text-sm lg:text-base font-poppins font-semibold text-secondary flex flex-col justify-center items-center p-4">
+    <div className="text-sm lg:text-base font-poppins font-semibold text-secondary flex flex-col justify-center items-center p-4 mb-16 lg:mb-0">
       <p className="lg:mr-2">
         Made by{" "}
         <a
