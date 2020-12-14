7550f3fee7d7154d126d4e53bf47daa02b1f3c47
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-12-14 05:42:10 +0530

    used followbtn comp and removed extra code

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,71 +1,19 @@
-import CircularProgress from "@material-ui/core/CircularProgress";
 import PersonAddIcon from "@material-ui/icons/PersonAdd";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import UserContext from "../../context/UserContext";
-import firebase from "../../firebase/init";
 import { useFollowers } from "../../hooks/useFollowers";
 import { useFollowings } from "../../hooks/useFollowings";
 import Avatar from "../Avatar/Avatar";
+import FollowButton from "../FollowButton/FollowButton";
 import Modal from "../Modal/Modal";
 
 const UserInfo = ({ fetchedUser }) => {
   const { user } = useContext(UserContext);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalLoading, setIsModalLoading] = useState(true);
 
-  const [isFollowing, setIsFollowing] = useState(false);
-  const [connectionDocID, setFollowingDocID] = useState("");
-
-  const { followers, isFollowersLoading, getFollowers } = useFollowers(
-    fetchedUser.uid
-  );
-  const { followings, isFollowingsLoading, getFollowings } = useFollowings(
-    fetchedUser.uid
-  );
-
-  const startFollowing = () => {
-    if (!user) {
-      alert("You need to sign in for that");
-      return;
-    }
-    const { id } = firebase.firestore().collection("connections").add({
-      followerID: user.uid,
-      followeeID: fetchedUser.uid,
-    });
-    setFollowingDocID(id);
-    setIsFollowing(true);
-  };
-
-  const stopFollowing = () => {
-    if (!user) {
-      alert("You need to sign in for that");
-      return;
-    }
-    firebase
-      .firestore()
-      .collection("connections")
-      .doc(connectionDocID)
-      .delete();
-    setIsFollowing(false);
-  };
-
-  useEffect(() => {
-    if (user) {
-      async function checkFollowing() {
-        const result = await firebase
-          .firestore()
-          .collection("connections")
-          .where("followeeID", "==", fetchedUser.uid)
-          .where("followerID", "==", user.uid)
-          .get();
-        if (result.size === 1) {
-          setIsFollowing(true);
-          setFollowingDocID(result.docs[0].id);
-        }
-      }
-
-      checkFollowing();
-    }
-  }, [fetchedUser, user]);
+  const { followers, getFollowers } = useFollowers(fetchedUser.uid);
+  const { followings, getFollowings } = useFollowings(fetchedUser.uid);
 
   return (
     <div className="h-full flex flex-col lg:flex-row lg:items-center rounded-xl bg-white w-full">
@@ -88,6 +36,7 @@ const UserInfo = ({ fetchedUser }) => {
               onClick={async () => {
                 setIsModalOpen(true);
                 await getFollowers();
+                setIsModalLoading(false);
               }}>
               {fetchedUser.followersCount}
               <span className="m-1 text-gray-500">Followers</span>
@@ -102,33 +51,16 @@ const UserInfo = ({ fetchedUser }) => {
           {user && fetchedUser.username === user.username ? (
             <button
               className="lg:mr-0 lg:ml-auto bg-primary text-white px-2 py-4  lg:px-8 lg:py-4 rounded-md"
-              type="submit"
-              onClick={() => startFollowing()}>
+              type="submit">
               <span className="mx-2">
                 <PersonAddIcon />
               </span>
               Edit Profile
             </button>
-          ) : isFollowing ? (
-            <button
-              className="lg:mr-0 lg:ml-auto bg-primary text-white px-2 py-4  lg:px-8 lg:py-4 rounded-md"
-              type="submit"
-              onClick={() => stopFollowing()}>
-              <span className="mx-2">
-                <PersonAddIcon />
-              </span>
-              Following
-            </button>
           ) : (
-            <button
-              className="lg:mr-0 lg:ml-auto bg-primary text-white px-2 py-4  lg:px-8 lg:py-4 rounded-md"
-              type="submit"
-              onClick={() => startFollowing()}>
-              <span className="mx-2">
-                <PersonAddIcon />
-              </span>
-              Follow
-            </button>
+            <div className="mr-0 ml-auto">
+              <FollowButton userID={fetchedUser.uid} />
+            </div>
           )}
         </div>
         <div className="flex justify-center lg:block">
@@ -151,13 +83,11 @@ const UserInfo = ({ fetchedUser }) => {
             top: "0",
             transform: "translate(-50%, 20%)",
           }}>
-          {followers ? (
-            <Modal users={followers} close={() => setIsModalOpen(false)} />
-          ) : (
-            <div className="flex justify-center">
-              <CircularProgress />
-            </div>
-          )}
+          <Modal
+            users={followers}
+            close={() => setIsModalOpen(false)}
+            loading={isModalLoading}
+          />
         </div>
       )}
     </div>
