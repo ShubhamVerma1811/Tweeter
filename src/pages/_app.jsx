db49e5527f56caa24395dd748a0100bf7d089edb
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-12-14 05:39:34 +0530

    added protect routes and pages context

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import BookmarksTweetsContext from "../context/BookmarksTweetsContext";
+import ExploreTweetsContext from "../context/ExploreTweetsContext";
 import HomeTweetsContext from "../context/HomeTweetsContext";
 import UserContext from "../context/UserContext";
 import firebase from "../firebase/init";
@@ -9,8 +11,13 @@ import "../styles/reset.css";
 const db = firebase.firestore();
 
 function MyApp({ Component, pageProps }) {
+  const Router = useRouter();
+  const protectedRoutes = ["/home", "/bookmarks"];
+
   const [user, setUser] = useState(null);
   const [homeTweetsContext, setHomeTweetsContext] = useState(null);
+  const [exploreTweetsContext, setExploreTweetsContext] = useState(null);
+  const [bookmarksTweetsContext, setBookmarksTweetsContext] = useState(null);
 
   useEffect(() => {
     async function getCurrentUser(userID) {
@@ -20,6 +27,7 @@ function MyApp({ Component, pageProps }) {
 
     firebase.auth().onAuthStateChanged((loggedUser) => {
       if (!loggedUser) {
+        if (protectedRoutes.includes(Router.pathname)) Router.push("/");
         setUser(null);
       } else {
         getCurrentUser(loggedUser.uid);
@@ -33,7 +41,13 @@ function MyApp({ Component, pageProps }) {
       <UserContext.Provider value={{ user, setUser }}>
         <HomeTweetsContext.Provider
           value={{ homeTweetsContext, setHomeTweetsContext }}>
-          <Component {...pageProps} />
+          <ExploreTweetsContext.Provider
+            value={{ exploreTweetsContext, setExploreTweetsContext }}>
+            <BookmarksTweetsContext.Provider
+              value={{ bookmarksTweetsContext, setBookmarksTweetsContext }}>
+              <Component {...pageProps} />
+            </BookmarksTweetsContext.Provider>
+          </ExploreTweetsContext.Provider>
         </HomeTweetsContext.Provider>
       </UserContext.Provider>
     </>
