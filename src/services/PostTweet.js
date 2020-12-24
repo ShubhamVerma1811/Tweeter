f0b813220c6bbd1858b33f0ab4b25b75c30be822
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-12-24 15:45:37 +0530

    fixed firestore timestamp

--- src/services/PostTweet.js
+++ src/services/PostTweet.js
@@ -1,12 +1,13 @@
 import firebase from "../firebase/init";
 
 const postTweet = async (authorId, text, imgLink, parentTweet = null) => {
+  console.log(authorId, text, imgLink, parentTweet);
   await firebase.firestore().collection("tweets").add({
     authorId,
     text,
     parentTweet,
     imgLink,
-    createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    createdAt: firebase.firestore.Timestamp.now(),
   });
 };
 
