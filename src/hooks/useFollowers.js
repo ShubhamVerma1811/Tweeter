8357c793625b1f434c59410b7d41988cef3bad08
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-12-25 12:12:29 +0530

    auth user typechecking

--- src/hooks/useFollowers.js
+++ src/hooks/useFollowers.js
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import UserContext from "../context/UserContext";
 import { fetchUser, fetchUserFollowers } from "../services/FetchData";
 
-export function useFollowers(userId) {
+export function useFollowers(userId, authUserID) {
+  const { user } = useContext(UserContext);
   const [followers, setFollowers] = React.useState([]);
   const [isFollowersLoading, setIsFollowersLoading] = React.useState(true);
 
   const getFollowers = async () => {
     const data = [];
     setIsFollowersLoading(true);
-    const followersSnapShot = await fetchUserFollowers(userId);
-    for (let i = 0; i < followersSnapShot.size; i++) {
-      data.push(
-        await fetchUser({
-          userID: followersSnapShot.docs[i].data().followerID,
-        })
-      );
+    if (user) {
+      const followersSnapShot = await fetchUserFollowers(userId, authUserID);
+      for (let i = 0; i < followersSnapShot.size; i++) {
+        data.push(
+          await fetchUser({
+            userID: followersSnapShot.docs[i].data().followerID,
+          })
+        );
+      }
     }
-    console.log(data);
     setFollowers(data);
     setIsFollowersLoading(false);
   };
